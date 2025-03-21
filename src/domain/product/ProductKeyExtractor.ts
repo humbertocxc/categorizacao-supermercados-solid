d847f16b74d5fbc53e9120ff468b51c15b2e9865
humberto <humberto.gnm98@gmail.com>
2025-03-20 22:05:24 -0300

    fix: different brands and types on same category error fixed

--- src/domain/product/ProductKeyExtractor.ts
+++ src/domain/product/ProductKeyExtractor.ts
@@ -6,13 +6,9 @@ import { normalizeString } from '../../utils/stringUtils';
 export class ProductKeyExtractor {
     public weightPattern: RegExp = /(\d+)\s*(kg|g|quilo|quilos|grama|gramas)?/i;
     public volumePattern: RegExp = /(\d+)\s*(l|litro|litros|ml|mililitro|mililitros)?/i;
-    public types: Set<string> = new Set(['integral', 'desnatado', 'branco', 'carioca', 'semi-desnatado']);
-    public brands: Set<string> = new Set(['piracanjuba', 'italac', 'parmalat', 'camil', 'tio joao']);
     public ignoredWords: Set<string> = new Set(['tipo']);
     public size: string = '';
-    public brand: string = '';
-    public type: string = '';
-    public description: string[] = [];
+    public significantWords: Set<string> = new Set();
     private processors: WordProcessor[];
 
     constructor(public words: string[]) {
@@ -26,24 +22,16 @@ export class ProductKeyExtractor {
         const processedWords = new Set<string>();
 
         this.words.forEach((word: string, index: number) => {
-            if (!this.ignoredWords.has(word)) {
+            if (!this.ignoredWords.has(word) && !processedWords.has(word)) {
                 this.processors.forEach(processor => processor.process(word, index));
+                if (!this.size || !this.size.startsWith(word)) {
+                    this.significantWords.add(word);
+                }
                 processedWords.add(word);
             }
         });
 
-        const selectedTypes = Array.from(this.types).filter(type => this.words.includes(type));
-        if (selectedTypes.length > 1) {
-            throw new Error(`Multiple types found: ${selectedTypes.join(', ')}`);
-        }
-        this.type = selectedTypes[0] || '';
-
-        const unprocessedWords = this.words.filter(word => !processedWords.has(word) && !this.ignoredWords.has(word));
-        if (unprocessedWords.length > 0) {
-            throw new Error(`Different words found: ${unprocessedWords.join(', ')}`);
-        }
-
-        return `${this.brand}-${this.type}-${this.description.join('-')}-${this.size}`
+        return `${Array.from(this.significantWords).sort().join('-')}-${this.size}`
             .replace(/-+/g, '-')
             .replace(/^-|-$/g, '');
     }
